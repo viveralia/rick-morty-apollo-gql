98faa681430a38a5d3c29a62d8adc3e26a1655cf
viveralia <edgarv10@gmail.com>
2020-02-14 17:40:29 -0600

    First commit 🎉

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// 4. Import gql and the useQuery hook
+import { gql } from "apollo-boost";
+import { useQuery } from "react-apollo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// --------------------------------
+// 5. Building the Query
+// --------------------------------
+const getAllCharacters = gql`
+  query getAllCharacters {
+    characters {
+      results {
+        id
+        name
+        image
+        origin {
+          name
+        }
+      }
+    }
+  }
+`;
+
+// --------------------------------
+// Main Component
+// --------------------------------
+const App = () => {
+  // 6. Use the useQuery hook and destructure the loading, error and data states
+  const { loading, error, data } = useQuery(getAllCharacters);
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <h1>Error</h1>
+        <pre>{JSON.stringify(error, null, 2)}</pre>
+      </div>
+    );
+  }
+
+  if (data) {
+    const { results: characters } = data.characters;
+    return (
+      <div>
+        {characters.map(({ id, name, image, origin: { name: origin } }) => (
+          <article key={id}>
+            <img src={image} alt={name} />
+            <h2>{name}</h2>
+            <p>
+              <strong>Origin: </strong>
+              {origin}
+            </p>
+          </article>
+        ))}
+      </div>
+    );
+  }
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { render } from "react-dom";
+import App from "./App";
+// 1. Import Apollo Client and Provider
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 2. Set a new client
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql" //URL of the GraphQL server
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// 3. Wrap the App Component in the Provider and pass down the client
+render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.querySelector("#root")
+);
